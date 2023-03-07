8f00820d5bcd8549eef6113395d0c0d8aa06c6dc
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-07 23:50:48 +0530

    [Jyoti Rao] Create connection with database

---
+++ EmployeePayrollAdo.NetProblem/EmployeeRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollAdo.NetProblem
+{
+    /// <summary>
+    /// UC1- Create connection with database
+    /// </summary>
+    public class EmployeeRepository
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static SqlConnection sqlConnection = null;
+        public static void GetAllEmployees()
+        {
+            try
+            {
+                EmployeePayroll model = new EmployeePayroll();
+                sqlConnection = new SqlConnection(connectionString);
+                string query = "select * from Employee_Payroll";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                Console.WriteLine("Connections is established successfully.....");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ EmployeePayrollAdo.NetProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollAdo.NetProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select given options");
+                Console.WriteLine("1.CreateConnectionsWithDatabase\n" +
+                    "8.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        EmployeeRepository.GetAllEmployees();
+                        Console.Write("\nPress any key to continue...... ");
+                        break;
+                    case 8:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please select only given options");
+                        Console.Write("\nPress any key to continue...... ");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
